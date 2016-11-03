ffd353fcec09496c7b32b4d469d80be97220d923
Cosdaman <Cosdaguy@gmail.com>
2016-11-03 14:45:09 +0800

    imported to github
    git

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/ReadExcel.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class ReadExcel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ChooseFileBtn = new System.Windows.Forms.Button();
+            this.CloseBtn = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.ConvXMLbtn = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.ConvXMLbtn2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ChooseFileBtn
+            // 
+            this.ChooseFileBtn.Location = new System.Drawing.Point(12, 12);
+            this.ChooseFileBtn.Name = "ChooseFileBtn";
+            this.ChooseFileBtn.Size = new System.Drawing.Size(335, 90);
+            this.ChooseFileBtn.TabIndex = 0;
+            this.ChooseFileBtn.Text = "Choose and Display Excel File";
+            this.ChooseFileBtn.UseVisualStyleBackColor = true;
+            this.ChooseFileBtn.Click += new System.EventHandler(this.ChooseFileBtn_Click);
+            // 
+            // CloseBtn
+            // 
+            this.CloseBtn.Location = new System.Drawing.Point(12, 182);
+            this.CloseBtn.Name = "CloseBtn";
+            this.CloseBtn.Size = new System.Drawing.Size(242, 53);
+            this.CloseBtn.TabIndex = 1;
+            this.CloseBtn.Text = "Close Program";
+            this.CloseBtn.UseVisualStyleBackColor = true;
+            this.CloseBtn.Click += new System.EventHandler(this.CloseBtn_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 241);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 33;
+            this.dataGridView2.Size = new System.Drawing.Size(1140, 429);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // ConvXMLbtn
+            // 
+            this.ConvXMLbtn.Location = new System.Drawing.Point(353, 12);
+            this.ConvXMLbtn.Name = "ConvXMLbtn";
+            this.ConvXMLbtn.Size = new System.Drawing.Size(335, 90);
+            this.ConvXMLbtn.TabIndex = 3;
+            this.ConvXMLbtn.Text = "Convert to XML (All)";
+            this.ConvXMLbtn.UseVisualStyleBackColor = true;
+            this.ConvXMLbtn.Click += new System.EventHandler(this.ConvXMLbtn_Click);
+            // 
+            // ConvXMLbtn2
+            // 
+            this.ConvXMLbtn2.Location = new System.Drawing.Point(694, 12);
+            this.ConvXMLbtn2.Name = "ConvXMLbtn2";
+            this.ConvXMLbtn2.Size = new System.Drawing.Size(335, 90);
+            this.ConvXMLbtn2.TabIndex = 4;
+            this.ConvXMLbtn2.Text = "Convert to XML (Cost Only)";
+            this.ConvXMLbtn2.UseVisualStyleBackColor = true;
+            this.ConvXMLbtn2.Click += new System.EventHandler(this.ConvXMLbtn2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(197, 388);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(116, 21);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.Visible = false;
+            // 
+            // ReadExcel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1164, 682);
+            this.Controls.Add(this.ConvXMLbtn2);
+            this.Controls.Add(this.ConvXMLbtn);
+            this.Controls.Add(this.CloseBtn);
+            this.Controls.Add(this.ChooseFileBtn);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "ReadExcel";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ChooseFileBtn;
+        private System.Windows.Forms.Button CloseBtn;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button ConvXMLbtn;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button ConvXMLbtn2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ WindowsFormsApplication2/WindowsFormsApplication2/ReadExcel.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Xml;
+
+namespace WindowsFormsApplication2
+{
+    public partial class ReadExcel : Form
+    {
+
+        string filePath = string.Empty;
+        string fileExt = string.Empty;
+        string con;
+        OleDbConnection conn;
+        OleDbCommand oconn;
+        OleDbDataAdapter sda;
+        DataTable data;
+
+        public ReadExcel()
+        {
+            InitializeComponent();
+        }
+
+        private void ChooseFileBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            if (file.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                filePath = file.FileName;
+                fileExt = Path.GetExtension(filePath);
+                if (fileExt.CompareTo(".xls") == 0 || fileExt.CompareTo(".xlsx") == 0)
+                {
+                    try
+                    {
+                        if (fileExt.CompareTo(".xls") == 0)
+                            con = @"provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filePath + ";Extended Properties='Excel 8.0;HRD=Yes;IMEX=1';"; //for below excel 2007  
+                        else
+                            con = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filePath + ";Extended Properties='Excel 12.0;HDR=Yes;IMEX=1';"; //for above excel 2007  
+
+                        conn = new OleDbConnection(con);
+                        oconn = new OleDbCommand("SELECT * FROM [Sheet1$]", conn);
+                        conn.Open();
+                        sda = new OleDbDataAdapter(oconn);
+                        DataTable data = new DataTable();
+                        sda.Fill(data);
+                        dataGridView2.DataSource = data;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please choose .xls or .xlsx file only.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error); //custom messageBox to show error  
+                }
+            }
+        }
+
+        private void CloseBtn_Click(object sender, EventArgs e)
+        {
+            this.Close(); //to close the window(Form1)
+        }
+
+        private void ConvXMLbtn_Click(object sender, EventArgs e)
+        {
+            if (dataGridView2.Rows.Count == 0)
+            {
+                MessageBox.Show("Please load an Excel file before attempting to convert to XML.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                saveFileDialog1.DefaultExt = "xml";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    saveFileDialog1_FileOk();
+                }
+            }
+        }
+
+        private void saveFileDialog1_FileOk()
+        {
+            string name = saveFileDialog1.FileName;
+
+            string[,] tempDataTable = new string[dataGridView2.Rows.Count, 8];
+            int rowNumber = 0;
+
+            foreach (DataGridViewRow row in dataGridView2.Rows)
+            {
+                tempDataTable[rowNumber, 0] = row.Cells["StockCode"].Value.ToString();
+                tempDataTable[rowNumber, 1] = row.Cells["StockUom"].Value.ToString();
+                tempDataTable[rowNumber, 2] = row.Cells["AlternateUom"].Value.ToString();
+                tempDataTable[rowNumber, 3] = row.Cells["OtherUom"].Value.ToString();
+                tempDataTable[rowNumber, 4] = row.Cells["ConvFactAltUom"].Value.ToString();
+                tempDataTable[rowNumber, 5] = row.Cells["ConvMulDiv"].Value.ToString();
+                tempDataTable[rowNumber, 6] = row.Cells["ConvFactOthUom"].Value.ToString();
+                tempDataTable[rowNumber, 7] = row.Cells["MulDiv"].Value.ToString();
+                rowNumber++;
+            }
+            using (XmlWriter writer = XmlWriter.Create(name))
+            {
+                writer.WriteStartDocument();
+                writer.WriteStartElement("SetupInvMaster");
+                for (int i = 0; i < tempDataTable.GetLength(0); i++)
+                {
+                    writer.WriteStartElement("Item");
+                    writer.WriteStartElement("Key");
+                    writer.WriteElementString("StockCode", tempDataTable[i,0]);
+                    writer.WriteEndElement();
+                    writer.WriteElementString("StockUom", tempDataTable[i, 1]);
+                    writer.WriteElementString("AlternateUom", tempDataTable[i, 2]);
+                    writer.WriteElementString("OtherUom", tempDataTable[i, 3]);
+                    writer.WriteElementString("ConvFactAltUom", tempDataTable[i, 4]);
+                    writer.WriteElementString("ConvMulDiv", tempDataTable[i, 5]);
+                    writer.WriteElementString("ConvFactOthUom", tempDataTable[i, 6]);
+                    writer.WriteElementString("MulDiv", tempDataTable[i, 7]);
+                    writer.WriteEndElement();
+                }
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+                MessageBox.Show("Generation of XML file has finished.","Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void ConvXMLbtn2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView2.Rows.Count == 0)
+            {
+                MessageBox.Show("Please load an Excel file before attempting to convert to XML.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                saveFileDialog1.DefaultExt = "xml";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    saveFileDialog2_FileOk();
+                }
+            }
+        }
+
+        private void saveFileDialog2_FileOk()
+        {
+            string name = saveFileDialog1.FileName;
+
+            conn = new OleDbConnection(con);
+            oconn = new OleDbCommand("SELECT * FROM [Sheet1$] WHERE cost<>0", conn);
+            conn.Open();
+            sda = new OleDbDataAdapter(oconn);
+            data = new DataTable();
+            sda.Fill(data);
+            dataGridView1.DataSource = data;
+
+            string[,] tempDataTable = new string[dataGridView1.Rows.Count, 3];
+            int rowNumber = 0;
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                    tempDataTable[rowNumber, 0] = row.Cells["Warehouse"].Value.ToString();
+                    tempDataTable[rowNumber, 1] = row.Cells["StockCode"].Value.ToString();
+                    tempDataTable[rowNumber, 2] = row.Cells["Cost"].Value.ToString();
+                    rowNumber++;
+            }
+
+            using (XmlWriter writer = XmlWriter.Create(name))
+            {
+                writer.WriteStartDocument();
+                writer.WriteStartElement("PostInvCostChange");
+                for (int i = 0; i < tempDataTable.GetLength(0); i++)
+                {
+                    writer.WriteStartElement("Item");
+                    writer.WriteElementString("Warehouse", tempDataTable[i, 0]);
+                    writer.WriteElementString("StockCode", tempDataTable[i, 1]);
+                    writer.WriteElementString("NewUnitCost", tempDataTable[i, 2]);
+                    writer.WriteElementString("UpdateLastCost", "Y");
+                    writer.WriteElementString("Reference", "UOM CHG");
+                    writer.WriteElementString("Notation", "Cost change Note");
+                    writer.WriteEndElement();
+                }
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+                MessageBox.Show("Generation of XML file has finished.", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
+
