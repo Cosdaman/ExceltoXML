0ed83ddc82dccd150a0017958f167abe17481ace
Cosdaman <Cosdaguy@gmail.com>
2016-11-09 14:10:01 +0800

    yep Revert "updated"
    This reverts commit f9987bf36756c808569ce58d22f602c6ecd118ae.

--- WindowsFormsApplication2/WindowsFormsApplication2/ReadExcel.cs
+++ WindowsFormsApplication2/WindowsFormsApplication2/ReadExcel.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data;
 using System.Data.OleDb;
-using System.Diagnostics;
+using System.Drawing;
 using System.IO;
+<<<<<<< HEAD
 using System.Text.RegularExpressions;
+=======
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+>>>>>>> parent of f9987bf... updated
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 using System.Xml;
 
 namespace WindowsFormsApplication2
@@ -12,17 +21,21 @@ namespace WindowsFormsApplication2
     public partial class ReadExcel : Form
     {
 
-        string excelFilePath = string.Empty;
+        string filePath = string.Empty;
         string fileExt = string.Empty;
         string con;
         OleDbConnection conn;
         OleDbCommand oconn;
         OleDbDataAdapter sda;
+<<<<<<< HEAD
         int numEntries;
         string filePath = string.Empty;
         String worksheetName;
         Decimal result;
 
+=======
+        DataTable data;
+>>>>>>> parent of f9987bf... updated
 
         public ReadExcel()
         {
@@ -32,19 +45,24 @@ namespace WindowsFormsApplication2
         private void ChooseFileBtn_Click(object sender, EventArgs e)
         {
             OpenFileDialog file = new OpenFileDialog();
-            if (file.ShowDialog() == DialogResult.OK)
+            if (file.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
+<<<<<<< HEAD
                 excelFilePath = file.FileName;
                 fileExt = Path.GetExtension(excelFilePath);
                 Cursor = Cursors.WaitCursor;
+=======
+                filePath = file.FileName;
+                fileExt = Path.GetExtension(filePath);
+>>>>>>> parent of f9987bf... updated
                 if (fileExt.CompareTo(".xls") == 0 || fileExt.CompareTo(".xlsx") == 0)
                 {
                     try
                     {
                         if (fileExt.CompareTo(".xls") == 0)
-                            con = @"provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + excelFilePath + ";Extended Properties='Excel 8.0;HRD=Yes;IMEX=1';"; //for below excel 2007  
+                            con = @"provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filePath + ";Extended Properties='Excel 8.0;HRD=Yes;IMEX=1';"; //for below excel 2007  
                         else
-                            con = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + excelFilePath + ";Extended Properties='Excel 12.0;HDR=Yes;IMEX=1';"; //for above excel 2007  
+                            con = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filePath + ";Extended Properties='Excel 12.0;HDR=Yes;IMEX=1';"; //for above excel 2007  
 
                         Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
                         Microsoft.Office.Interop.Excel.Workbook workbook = app.Workbooks.Open(excelFilePath);
@@ -78,14 +96,11 @@ namespace WindowsFormsApplication2
 
         private void CloseBtn_Click(object sender, EventArgs e)
         {
-            this.Close();
+            this.Close(); //to close the window(Form1)
         }
 
         private void ConvXMLbtn_Click(object sender, EventArgs e)
         {
-            
-            Int32.TryParse(textBox1.Text, out numEntries);
-
             if (dataGridView2.Rows.Count == 0)
             {
                 MessageBox.Show("Please load an Excel file before attempting to convert to XML.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -103,8 +118,7 @@ namespace WindowsFormsApplication2
         private void saveFileDialog1_FileOk()
         {
             string name = saveFileDialog1.FileName;
-            string nameNoExt = Path.GetFileNameWithoutExtension(name);
-            filePath = Path.GetDirectoryName(name);
+
             string[,] tempDataTable = new string[dataGridView2.Rows.Count, 8];
             int rowNumber = 0;
             
@@ -121,21 +135,22 @@ namespace WindowsFormsApplication2
                 tempDataTable[rowNumber, 7] = row.Cells[13].Value.ToString();
                 rowNumber++;
             }
+<<<<<<< HEAD
 
             float numFiles = (tempDataTable.GetLength(0) / numEntries);
             int counter; 
             int rows = tempDataTable.GetLength(0);
 
             for (int x = 0; x < numFiles+1; x++)
+=======
+            using (XmlWriter writer = XmlWriter.Create(name))
+>>>>>>> parent of f9987bf... updated
             {
-               
-                if (!Directory.Exists(filePath + "/" + nameNoExt + "/"))
+                writer.WriteStartDocument();
+                writer.WriteStartElement("SetupInvMaster");
+                for (int i = 0; i < tempDataTable.GetLength(0); i++)
                 {
-                    Directory.CreateDirectory(filePath + "/" + nameNoExt + "/");
-                }
-
-                using (XmlWriter writer = XmlWriter.Create(filePath + "/" + nameNoExt +  "/" + nameNoExt + (x + 1) + ".xml"))
-                {
+<<<<<<< HEAD
                     writer.WriteStartDocument();
                     writer.WriteStartElement("SetupInvMaster");
                     for (int i = 0; i < numEntries; i++)
@@ -168,17 +183,29 @@ namespace WindowsFormsApplication2
                         }
 
                     }
+=======
+                    writer.WriteStartElement("Item");
+                    writer.WriteStartElement("Key");
+                    writer.WriteElementString("StockCode", tempDataTable[i,0]);
                     writer.WriteEndElement();
-                    writer.WriteEndDocument();
+                    writer.WriteElementString("StockUom", tempDataTable[i, 1]);
+                    writer.WriteElementString("AlternateUom", tempDataTable[i, 2]);
+                    writer.WriteElementString("OtherUom", tempDataTable[i, 3]);
+                    writer.WriteElementString("ConvFactAltUom", tempDataTable[i, 4]);
+                    writer.WriteElementString("ConvMulDiv", tempDataTable[i, 5]);
+                    writer.WriteElementString("ConvFactOthUom", tempDataTable[i, 6]);
+                    writer.WriteElementString("MulDiv", tempDataTable[i, 7]);
+>>>>>>> parent of f9987bf... updated
+                    writer.WriteEndElement();
                 }
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+                MessageBox.Show("Generation of XML file has finished.","Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
-            MessageBox.Show("Generation of XML files finished.", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private void ConvXMLbtn2_Click(object sender, EventArgs e)
         {
-            Int32.TryParse(textBox1.Text, out numEntries);
-
             if (dataGridView2.Rows.Count == 0)
             {
                 MessageBox.Show("Please load an Excel file before attempting to convert to XML.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -197,6 +224,7 @@ namespace WindowsFormsApplication2
         {
             Cursor = Cursors.WaitCursor;
             string name = saveFileDialog1.FileName;
+<<<<<<< HEAD
             string nameNoExt = Path.GetFileNameWithoutExtension(name);
             filePath = Path.GetDirectoryName(name);
             //conn = new OleDbConnection(con);
@@ -215,6 +243,8 @@ namespace WindowsFormsApplication2
             //DataTable data = new DataTable();
             //sda.Fill(data);
             //dataGridView2.DataSource = data;
+=======
+>>>>>>> parent of f9987bf... updated
 
             string[,] tempDataTable = new string[dataGridView2.Rows.Count, 3];
             int rowNumber = 0;
@@ -222,6 +252,7 @@ namespace WindowsFormsApplication2
 
             foreach (DataGridViewRow row in dataGridView2.Rows)
             {
+<<<<<<< HEAD
                 checker = row.Cells[109].Value.ToString();
                 //if (!string.IsNullOrEmpty(checker))
                 decimal.TryParse(checker, out result);
@@ -241,37 +272,30 @@ namespace WindowsFormsApplication2
             int rows = tempDataTable.GetLength(0);
 
             for (int x = 0; x < numFiles + 1; x++)
+=======
+                    tempDataTable[rowNumber, 0] = row.Cells["Warehouse"].Value.ToString();
+                    tempDataTable[rowNumber, 1] = row.Cells["StockCode"].Value.ToString();
+                    tempDataTable[rowNumber, 2] = row.Cells["Cost"].Value.ToString();
+                    rowNumber++;
+            }
+
+            using (XmlWriter writer = XmlWriter.Create(name))
+>>>>>>> parent of f9987bf... updated
             {
-                if (!Directory.Exists(filePath + "/" + nameNoExt + "/"))
+                writer.WriteStartDocument();
+                writer.WriteStartElement("PostInvCostChange");
+                for (int i = 0; i < tempDataTable.GetLength(0); i++)
                 {
-                    Directory.CreateDirectory(filePath + "/" + nameNoExt + "/");
-                }
-
-                using (XmlWriter writer = XmlWriter.Create(filePath + "/" + nameNoExt + "/" + nameNoExt + (x + 1) + ".xml"))
-                {
-                    writer.WriteStartDocument();
-                    writer.WriteStartElement("PostInvCostChange");
-                    for (int i = 0; i < numEntries; i++)
-                    {
-                        counter = i + x * numEntries;
-
-                        writer.WriteStartElement("Item");
-                        writer.WriteElementString("Warehouse", tempDataTable[i, 0]);
-                        writer.WriteElementString("StockCode", tempDataTable[i, 1]);
-                        writer.WriteElementString("NewUnitCost", tempDataTable[i, 2]);
-                        writer.WriteElementString("UpdateLastCost", "Y");
-                        writer.WriteElementString("Reference", "UOM CHG");
-                        writer.WriteElementString("Notation", "Cost change Note");
-                        writer.WriteEndElement();
-
-                        if (counter + 2 > rows)
-                        {
-                            i = numEntries + 10;
-                        }
-                    }
+                    writer.WriteStartElement("Item");
+                    writer.WriteElementString("Warehouse", tempDataTable[i, 0]);
+                    writer.WriteElementString("StockCode", tempDataTable[i, 1]);
+                    writer.WriteElementString("NewUnitCost", tempDataTable[i, 2]);
+                    writer.WriteElementString("UpdateLastCost", "Y");
+                    writer.WriteElementString("Reference", "UOM CHG");
+                    writer.WriteElementString("Notation", "Cost change Note");
                     writer.WriteEndElement();
-                    writer.WriteEndDocument();
                 }
+<<<<<<< HEAD
             }
             Cursor = Cursors.Default;
             MessageBox.Show("Generation of XML file has finished.", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
@@ -287,6 +311,11 @@ namespace WindowsFormsApplication2
             else
             {
                 MessageBox.Show("Please convert an XML file before attempting to open the directory.", "Error.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+=======
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+                MessageBox.Show("Generation of XML file has finished.", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+>>>>>>> parent of f9987bf... updated
             }
         }
     }
